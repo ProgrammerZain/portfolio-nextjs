c0ed47f4dfc35ca0daf78ac665c9f60cf822feda
Zain Irfan <zainirfan565@gmail.com>
2026-07-20 01:45:02 +0500

    chore: install and configure @netlify/vite-plugin in vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,12 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
+import netlify from "@netlify/vite-plugin";
 import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react(), tailwindcss(), netlify()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
